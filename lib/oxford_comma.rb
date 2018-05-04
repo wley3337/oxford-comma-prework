185fddf385dd5c3ea3a0b57eb8b7057aa7308ebc
wley3337@gmail.com <wley3337@gmail.com>
2018-05-04 22:23:09 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  if array.length == 1 
+    array.join()
+  elsif array.length == 2 
+    array.join(" and ")
+  else
+    almost_all_array = []
+    last_item_array = []
+    last_item_array.push("and " + array.pop)
+    array.each do |x|
+      almost_all_array.push(x + ", ")
+    end
+    almost_all_array.concat(last_item_array)
+    almost_all_array.join()
+  end
 end
